98e3fa84e73be6be9460b935ef29d18935169a82
yalh76 <yalh@yahoo.com>
2019-02-05 23:06:45 +0100

    remove ynh_cron_upgrade_nodenot used

--- scripts/_future.sh
+++ scripts/_future.sh
@@ -45,52 +45,7 @@ abort_if_up_to_date () {
     fi
 }
 
-ynh_cron_upgrade_node () {
-    # Build the update script
-    cat > "$n_install_dir/node_update.sh" << EOF
-#!/bin/bash
 
-version_path="$node_version_path"
-n_install_dir="$n_install_dir"
-
-# Log the date
-date
-
-# List all real installed version of node
-all_real_version="\$(find \$version_path/* -maxdepth 0 -type d | sed "s@\$version_path/@@g")"
-
-# Keep only the major version number of each line
-all_real_version=\$(echo "\$all_real_version" | sed 's/\..*\$//')
-
-# Remove double entries
-all_real_version=\$(echo "\$all_real_version" | sort --unique)
-
-# Read each major version
-while read version
-do
-    echo "Update of the version \$version"
-    sudo \$n_install_dir/bin/n \$version
-
-    # Find the last "real" version for this major version of node.
-    real_nodejs_version=\$(find \$version_path/\$version* -maxdepth 0 | sort --version-sort | tail --lines=1)
-    real_nodejs_version=\$(basename \$real_nodejs_version)
-
-    # Update the symbolic link for this version
-    sudo ln --symbolic --force --no-target-directory \$version_path/\$real_nodejs_version \$version_path/\$version
-done <<< "\$(echo "\$all_real_version")"
-EOF
-
-    chmod +x "$n_install_dir/node_update.sh"
-
-    # Build the cronjob
-    cat > "/etc/cron.daily/node_update" << EOF
-#!/bin/bash
-
-$n_install_dir/node_update.sh >> $n_install_dir/node_update.log
-EOF
-
-    chmod +x "/etc/cron.daily/node_update"
-}
 
 ynh_version_gt ()
 {
