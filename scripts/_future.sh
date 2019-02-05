54f3fa181ca7a288dbfc18a7173f7f7619c54bba
yalh76 <yalh@yahoo.com>
2019-02-05 23:24:11 +0100

    Remove not needed functions

--- scripts/_future.sh
+++ scripts/_future.sh
@@ -1,30 +1,3 @@
-
-
-log() {
-  echo "${1}"
-}
-
-info() {
-  log "[INFO] ${1}"
-}
-
-warn() {
-  log "[WARN] ${1}"
-}
-
-err() {
-  log "[ERR] ${1}"
-}
-to_logs() {
-
-  # When yunohost --verbose or bash -x
-  if $_ISVERBOSE; then
-    cat
-  else
-    cat > /dev/null
-  fi
-}
-
 ynh_version_gt ()
 {
     dpkg --compare-versions "$1" gt "$2"
