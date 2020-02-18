f631bf45a977bb17d49fa8dbd09736220deccf00
yalh76 <yalh@yahoo.com>
2020-02-18 23:06:11 +0100

    Fix upgrade

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 # dependencies used by the app
 pkg_dependencies="mongodb mongodb-server mongo-tools"
-nodejs_version=12.15.0
+nodejsversion=12.15.0
 
 #=================================================
 # PERSONAL HELPERS
