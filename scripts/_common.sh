642671d6410af8718ef092afd66c434516bbb782
yalh76 <yalh@yahoo.com>
2020-03-18 02:45:41 +0100

    Switch to MongoDB Community Edition
    Fix #78

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="mongodb mongodb-server mongo-tools"
+pkg_dependencies="mongodb-org mongodb-org-server mongo-org-tools"
 nodejsversion=12.15.0
 
 #=================================================
