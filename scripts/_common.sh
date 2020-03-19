190b806379b603bb5f36f6de887bef6d52b5edd3
yalh76 <yalh@yahoo.com>
2020-03-19 22:20:33 +0100

    fix dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,8 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="mongodb-org mongodb-org-server mongo-org-tools"
+pkg_dependencies="mongodb mongodb-server mongo-tools"
+pkg_dependencies_buster="mongodb-org mongodb-org-server mongo-org-tools"
 nodejsversion=12.15.0
 
 #=================================================
