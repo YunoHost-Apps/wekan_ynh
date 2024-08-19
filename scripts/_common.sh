49335f5cdcb7961b424ccf64450b5743518729d7
Mateusz <2871798+orhtej2@users.noreply.github.com>
2024-08-19 23:09:21 +0200

    Another fixup

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -15,12 +15,12 @@ ynh_sync_admin_user_to_wekan() {
     local tmp=$(mktemp -d)
 
     ynh_setup_source --dest_dir="$tmp"  --source_id="ldap"
+    ynh_add_config --template="add_admin_user.py" --destination="$tmp/add_admin_user.py"
 
     python3 -m venv $tmp
     source $tmp/bin/activate
     pip install pymongo ldap3 environs
     
-    ynh_add_config --template="add_admin_user.py" --destination="$tmp/add_admin_user.py"
     source "$install_dir/.env"
 
     python3 $tmp/add_admin_user.py
