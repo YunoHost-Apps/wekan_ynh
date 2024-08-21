23fde149339fb9ae2aed79d64bde05ebeefa88c2
Mateusz <2871798+orhtej2@users.noreply.github.com>
2024-08-21 21:57:09 +0200

    python3-dev for now

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -18,7 +18,7 @@ ynh_sync_admin_user_to_wekan() {
     ynh_add_config --template="add_admin_user.py" --destination="$tmp/add_admin_user.py"
 
     python3 -m venv $tmp
-    $tmp/bin/pip install pymongo python-ldap environs
+    $tmp/bin/pip install pymongo python-ldap environs python3-dev
     
     source "$install_dir/.env"
 
